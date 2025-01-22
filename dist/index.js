9a8494ed7d834a38bf68a3e7d044e40ce5c0df24
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-01-22 10:18:39 -0800

    functions

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-const small = 1;
-const medium = 2;
-const large = 3;
-let mySize = 2;
-console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-const small = 1;
-const medium = 2;
-const large = 3;
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022){
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
 
-// PascalCase, enum represents list of constants
-const enum Size { Small = 1, Medium, Large }
-let mySize: Size = Size.Medium;
-console.log(mySize);
-
+calculateTax(10_000);
