8a7677d06709d2b617d1fbe86518a675569d7ff3
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-01-22 10:11:10 -0800

    enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
-let user = [1, 'Mosh'];
+const small = 1;
+const medium = 2;
+const large = 3;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-let user: [number, string] = [1, 'Mosh'];
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// PascalCase, enum represents list of constants
+const enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
